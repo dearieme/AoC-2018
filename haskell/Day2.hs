d58ff268fcc716d9599ec3168a8d861f1758b0ab
dearieme <scott@dearie.me.uk>
2018-12-02 18:00:27 +0000

    day2: implement part 2

--- haskell/Day2.hs
+++ haskell/Day2.hs
@@ -1,6 +1,8 @@
 module Day2 where
 
-import qualified Data.Map as Map
+import           Data.List  (intersect)
+import qualified Data.Map   as Map
+import           Data.Maybe
 
 charFrequency :: String -> [Int]
 charFrequency xs = Map.elems $ go Map.empty xs
@@ -23,9 +25,35 @@ checksum xs = hasTwo * hasThree
     hasTwo = numWithFreq 2 xs
     hasThree = numWithFreq 3 xs
 
+diffId :: String -> String -> Int
+diffId id1 id2 = length differences
+  where
+    differences = filter (uncurry (/=)) $ zip id1 id2
+
+isDiffByOne :: String -> String -> Bool
+isDiffByOne id1 id2 = diffId id1 id2 == 1
+
+diffByOne :: Monad m => m String -> m (Maybe (String, String))
+diffByOne xs = do
+  id1 <- xs
+  id2 <- xs
+  if isDiffByOne id1 id2
+    then return $ Just (id1, id2)
+    else return Nothing
+
+diffByOnes :: [String] -> (String, String)
+diffByOnes xs = fromJust $ head $ filter isJust $ diffByOne xs
+
 main :: IO ()
 main = do
   input <- readFile "day2_inputs.txt"
   let inputList = lines input
   putStr "Checksum: "
   print $ checksum inputList
+
+  let pairDiffByOne = diffByOnes inputList
+  putStr "IDs differing by one char: "
+  print $ diffByOnes inputList
+  putStr "Common chars are: "
+  print $ uncurry intersect pairDiffByOne
+
