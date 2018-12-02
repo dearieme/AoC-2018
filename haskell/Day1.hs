08034710067b214b130dfa4a77641819da325312
dearieme <scott@dearie.me.uk>
2018-12-02 15:32:53 +0000

    day1: implement solution to part 2
    Implements solution to second part of the problem - find the first
    repeated frequency.

--- haskell/Day1.hs
+++ haskell/Day1.hs
@@ -1,13 +1,32 @@
 module Day1 where
 
+import qualified Data.Map   as Map
+import           Data.Maybe
+
 readChange :: String -> Int
 readChange ('+':xs) = read xs
 readChange xs       = read xs
 
-processChanges :: [String] -> Int
-processChanges xs = sum $ map readChange xs
+sumChanges :: [String] -> Int
+sumChanges xs = sum $ map readChange xs
+
+findFirstRepeat :: Map.Map Int Bool -> [Int] -> Int
+findFirstRepeat seen (x:xs) =
+  if isNothing $ Map.lookup x seen
+    then findFirstRepeat (Map.insert x True seen) xs
+    else x
+
+getRepeat :: [String] -> Int
+getRepeat xs = findFirstRepeat Map.empty runningTotals
+  where
+    cycledInputs = cycle $ map readChange xs
+    runningTotals = scanl (+) 0 cycledInputs
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print $ processChanges (lines input)
+  let inputList = lines input
+  putStr "Resulting frequency: "
+  print $ sumChanges inputList
+  putStr "First repeated frequency: "
+  print $ getRepeat inputList
